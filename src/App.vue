1c6740b78b71f286660c7f23374b41cda6b26aeb
enricoforesta <enrico.foresta97@gmail.com>
2023-12-14 13:28:18 +0100

    feat: milestone 0 fine

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,28 @@
-<script >
+<script>
+import { store } from './store'
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      store
+    }
+  },
+  created() {
+    this.callApi()
+  },
+  methods: {
+    callApi() {
+      const myUrl = `${this.store.apiUrl}&query=${this.store.searchText}`
+      console.log(myUrl)
+      axios.get(myUrl).then((response) => {
+        this.store.results = response.data.results
+        console.log(response.data.results)
+        console.log(this.store.results)
+      })
+    }
+  },
+
+}
 </script>
 
 <template></template>
