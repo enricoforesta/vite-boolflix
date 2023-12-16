bc3dc5d19b0b7e24cdd6ce1b79807890f00bb2eb
enricoforesta <enrico.foresta97@gmail.com>
2023-12-16 18:18:01 +0100

    script: Boostrap + scss

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -2,6 +2,7 @@ const config = {
     apiUrlMovies: 'https://api.themoviedb.org/3/search/movie',
     apiUrlSeries: 'https://api.themoviedb.org/3/search/tv',
     apiKey: 'e202549b488227586622af1897302300',
-    apiImage: 'https://image.tmdb.org/t/p/w342'
+    apiImage: 'https://image.tmdb.org/t/p/w342',
+    language: 'it-IT'
 }
 export default config
